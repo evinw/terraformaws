4c5b3b9cd0067b6937d32e0e28d39aa4bf7a2ca1
Evin Weissenberg <aimatrixdream@gmail.com>
2023-01-12 13:06:40 -0700

    Create tf.tf

---
+++ tf.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_security_group" "firewall" {
+  name        = "firewall"
+  description = "Firewall for the VPC"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
